558cc1017bd83e8cdc944c2e8fc1ff55c960b2f0
unknown <prakhar7799@gmail.com>
2020-10-15 23:14:49 +0530

    first commit

---
+++ src/app/predef-todo.ts
@@ -0,0 +1,7 @@
+import {Todo} from './todo';
+export const Todos: Todo[] = [
+{ id : 1, date : '12/1/2020', title : 'cricket', description : 'to play cricket' },
+{ id : 2, date : '12/2/2020', title : 'football', description : 'to play football'},
+{ id : 3, date : '12/3/2020', title : 'hockey', description : 'to play hockey'},
+{ id : 4, date : '12/4/2020', title : 'basketball', description : 'to play basketball'}
+];
